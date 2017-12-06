cc1cefd87d50e81412079b5ff155c59e2394f6e1
Chris Borodic <cborodic@cambridgesemantics.com>
2017-12-06 13:21:22 -0500

    removed debugger

--- src/listeners/TrigGrammerListener.js
+++ src/listeners/TrigGrammerListener.js
@@ -230,7 +230,6 @@ TrigGrammerListener.prototype.getDocument = function(){
     }
   };
   result.analysisErrors = result.analysisErrors.concat(this.finalize());
-  debugger;
   return result;
 };
 TrigGrammerListener.prototype.prefixId = function(ctx){};
